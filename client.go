577783c3bec88034354c6c54450bede825d66de4
Morten Jessen <mortenjessen5d@gmail.com>
2023-08-25 21:04:52 +0200

    chore: format

--- client.go
+++ client.go
@@ -58,7 +58,7 @@ func (c *tvTidClient) GetProgramDetails(channelId string, programId string) (*Pr
 	err := c.getFromJson(url, &programDetailsResponse)
 
 	if err != nil {
-        return nil, err
+		return nil, err
 	}
 
 	return &programDetailsResponse.Program, nil
